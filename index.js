e8132e3d8ab828e3282c1577e7ba6c9d29ce7afe
shinlee1213 <91505354+shinlee1213@users.noreply.github.com>
2021-11-15 15:31:41 -0600

    API integration
    API integration started for weekly forecast

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ function searchCity() {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "35ccbef0c279f2a123735a93a339afb4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showForecast);
+}
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#currentTemperature").innerHTML = Math.round(
@@ -57,6 +64,8 @@ function showWeather(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function showPosition(postion) {
@@ -90,7 +99,8 @@ celsius.addEventListener("click", showCelsius);
 let fahrenheit = document.querySelector("#fahrenheit-link");
 fahrenheit.addEventListener("click", showFahrenheit);
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
