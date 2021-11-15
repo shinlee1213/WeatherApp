6ed990ea962da7aaf3cfba1a9c182b7040684167
shinlee1213 <91505354+shinlee1213@users.noreply.github.com>
2021-11-15 12:34:10 -0600

    week forecast JS
    added JS to loop week forecast

--- index.js
+++ index.js
@@ -89,3 +89,31 @@ celsius.addEventListener("click", showCelsius);
 
 let fahrenheit = document.querySelector("#fahrenheit-link");
 fahrenheit.addEventListener("click", showFahrenheit);
+
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+            <div class="week-forecast-date">${day}</div>
+            <img
+              src="https://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="42"
+            />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">99°</span>
+              <span class="weather-forecast-temperature-min">70°</span>
+            </div>
+          </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+showForecast();
